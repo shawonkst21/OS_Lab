780a30b56b5b00d800b4bb64437bf8d39bdc863c
root <root@DESKTOP-19SNBOJ>
2025-05-19 20:18:38 +0600

    process creation using fork() system calls

---
+++ process.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<unistd.h>
+#include<sys/types.h>
+using namespace std;
+int main(){
+    pid_t q;
+    cout<<"Not create any process"<<endl;
+    q=fork();
+     
+     if(q<0){
+      cout<<"failed to create process"<<endl;
+      }
+      
+      else if(q==0){
+       
+       cout<<"my parent process pid "<<getppid()<<endl;
+         cout<<"i am child:"<<getpid()<<endl;
+       }
+       else{
+        cout<<"my process pid "<<getpid()<<endl;
+        cout<<"my child process pid "<<q<<endl;
+         }
+       cout<<"common"<<endl;
+}
+
+
